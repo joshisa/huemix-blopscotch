a5e5e1c1b7fbec67f6b7112c50a86239a6acbc27
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-01 17:04:51 -0400

    Create jstart.js

---
+++ tourlet/jstart.js
@@ -0,0 +1,20 @@
+var jstart = /^https?:\/\/(*.?)\.ibm\.com\/(.*?)\/ebusiness\/jstart\/(.*?)/;
+if (jstart.test(location.href)) {
+		console.log(prefix + "Loading tour button on IBM jStart Page");
+		// Let's attach a Take Tour button to the page
+		var mainsection = document.getElementById("ibm-content-nav");
+		var b = document.createElement('a');
+		b.setAttribute('id', 'startTourBtn');
+		b.setAttribute('class', 'ibm-btn-pri');
+		b.setAttribute('style', 'position:inherit;float:left;');
+		b.setAttribute('href', '#');
+		var t = document.createTextNode("Take Tour");
+		b.appendChild(t);
+		mainsection.appendChild(b);
+		console.log(prefix + "Tour Button injected ...");
+		
+		// Load the jstart website tour
+		var t = document.createElement ('script');
+		t.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/tour/jstart.js";
+		document.body.appendChild (t);
+}
