ae0d05ca5d8a0afe909f516f335e27fe3a43ed4d
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-01 16:08:35 -0400

    Update demo.js

--- tourlet/demo.js
+++ tourlet/demo.js
@@ -1,6 +1,6 @@
 var demo = /^file?:\/\/\/(.*?)hopscotch(.*?)/;
 if (demo.test(location.href)) {
 	var t = document.createElement ('script');
-	t.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/js/demo.js";
+	t.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/tourlet/demo.js";
 	document.body.appendChild (t);
 }
