3599ed18b26486d59de1ad003ee8e4fb733379c0
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-01 17:19:27 -0400

    Create jstart.js

---
+++ tour/jstart.js
@@ -0,0 +1,67 @@
+/* globals hopscotch: false */
+
+/* ============ */
+/* EXAMPLE TOUR */
+/* ============ */
+var tour = {
+  id: 'hello-hopscotch',
+  steps: [
+    {
+      target: 'ibm-leadspace-body',
+      title: 'Welcome to the IBM jStart Page!',
+      content: 'Hey there! Welcome to our home page.  There\'s alot to explore, so strap on your seatbelt and let\'s get started.',
+      placement: 'left',
+      arrowOffset: center
+    },
+  ],
+  showPrevButton: true,
+  scrollTopMargin: 100
+},
+
+/* ========== */
+/* TOUR SETUP */
+/* ========== */
+addClickListener = function(el, fn) {
+  if (el.addEventListener) {
+    el.addEventListener('click', fn, false);
+  }
+  else {
+    el.attachEvent('onclick', fn);
+  }
+},
+
+init = function() {
+  var startBtnId = 'startTourBtn',
+      calloutId = 'startTourCallout',
+      mgr = hopscotch.getCalloutManager(),
+      state = hopscotch.getState();
+
+  if (state && state.indexOf('hello-hopscotch:') === 0) {
+    // Already started the tour at some point!
+    hopscotch.startTour(tour);
+  }
+  else {
+    // Looking at the page for the first(?) time.
+    setTimeout(function() {
+      mgr.createCallout({
+        id: calloutId,
+        target: startBtnId,
+        placement: 'right',
+        title: 'Welcome digital traveller',
+        content: 'Start by taking a tour!',
+        yOffset: -25,
+        arrowOffset: 20,
+        width: 240
+      });
+    }, 100);
+  }
+
+  addClickListener(document.getElementById(startBtnId), function() {
+    if (!hopscotch.isActive) {
+      mgr.removeAllCallouts();
+      hopscotch.startTour(tour);
+    }
+  });
+};
+
+init();
