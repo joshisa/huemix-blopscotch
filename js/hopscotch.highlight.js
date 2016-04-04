7248b283b6110d9e47c1392b822baee5113605b6
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-04 18:37:34 -0400

    Update hopscotch.highlight.js

--- js/hopscotch.highlight.js
+++ js/hopscotch.highlight.js
@@ -92,7 +92,7 @@
     skipIfNoElement: true,
     isRtl:           false,
     cookieName:      'hopscotch.tour.state',
-    highlight:       true,
+    highlight:       false,
     highlightMargin: 0
   };
 
@@ -1125,8 +1125,6 @@
         bubbleWidth:    defaultOpts.bubbleWidth,
         bubblePadding:  defaultOpts.bubblePadding,
         arrowWidth:     defaultOpts.arrowWidth,
-        highlight:       defaultOpts.highlight,
-        highlightMargin: defaultOpts.highlightMargin,
         isRtl:          defaultOpts.isRtl,
         showNumber:     true,
         isTourBubble:   true
