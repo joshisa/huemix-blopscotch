9e93494f6ae1bf334b02a130353a306757d9f67f
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-05 03:08:15 -0400

    Update spark.js

--- tourlet/spark.js
+++ tourlet/spark.js
@@ -1,22 +1,28 @@
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+};
+
 var spark = /^https?:\/\/(console)\.ng\.bluemix\.net\/data\/analytics(.*?)/;
-if (spark.test(location.href)) {
-		console.log(prefix + "Loading tour button on Working with Notebooks and Spark Instances Dashboard");
-		// Let's attach a Take Tour button to the page
-		var newnotebookbutton = document.getElementsByClassName("button-area");
-		var b = document.createElement('div');
-		b.setAttribute('id', 'startTourBtn');
-		b.setAttribute('style', 'position:relative;display:inline-block;float:right;');
-		var a = document.createElement('a');
-		a.setAttribute('class','button primary');
-		a.setAttribute('style','width:100%;');
-		a.setAttribute('href', '#');
-		var t = document.createTextNode("▶ Tour");
-		a.appendChild(t);
-		b.appendChild(a);
-		newnotebookbutton[0].appendChild(b);
-		console.log(prefix + "Tour Button injected ...");
-		
-		var t = document.createElement ('script');
-		t.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/tour/spark.js";
-		document.body.appendChild (t);
-	}
+// Initialize tour if it's the user's first time
+if (spark.test(location.href) && !getCookie("toured")) {
+	console.log(prefix + "Loading tour button on Working with Notebooks and Spark Instances Dashboard");
+	// Let's attach a Take Tour button to the page
+	var newnotebookbutton = document.getElementsByClassName("button-area");
+	var b = document.createElement('div');
+	b.setAttribute('id', 'startTourBtn');
+	b.setAttribute('style', 'position:relative;display:inline-block;float:right;');
+	var a = document.createElement('a');
+	a.setAttribute('class','button primary');
+	a.setAttribute('style','width:100%;');
+	a.setAttribute('href', '#');
+	var t = document.createTextNode("▶ Tour");
+	a.appendChild(t);
+	b.appendChild(a);
+	newnotebookbutton[0].appendChild(b);
+	console.log(prefix + "Tour Button injected ...");
+	
+	var t = document.createElement ('script');
+	t.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/tour/spark.js";
+	document.body.appendChild (t);
+}
