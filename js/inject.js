98fce08c11f3ed4aa41111ddd5895c70d82abe27
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-06 17:11:19 -0400

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -33,7 +33,19 @@ chrome.extension.sendMessage({}, function(response) {
               }
               // Let's only load this stuff once for a given dom
           }).onFailure(function (status) {
-            alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
+              tourlets = ["tourlets/demo.js", "tourlets/jstart.js", "tourlets/jupyter.js", "tourlets/spark.js"];
+              console.log(prefix + "Number of Tourlets discovered : " + tourlets.length);
+              var i = 0;
+              while (tourlets[i]) {
+                  // https://rawgit.com/joshisa/huemix-blopscotch/master/ + tourlets.path
+                  console.log(prefix + "Loading " + tourlets[i].path);
+                  proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/' + tourlets[i].path).onSuccess(function (data) {
+                        eval(data);
+                  }).onFailure(function (status) {
+                       alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
+                  });
+                  i++;
+              }
           });
 
           console.log(prefix + "Hopscotch Dependencies successfully injected");
