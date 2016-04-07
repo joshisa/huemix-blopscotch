7923e0e00c9df9961017b0f43b419156a3c19182
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-06 22:15:18 -0400

    Update jupyter.js

--- tour/jupyter.js
+++ tour/jupyter.js
@@ -22,11 +22,18 @@ var tour = {
   id: 'hello-jupyter-on-bluemix',
   steps: [
     {
-      target: ['maintoolbar-container'],
+      target: document.querySelectorAll('.data-page-icon.analytics')[0],
+      title: 'Analytics',
+      content: 'Almost done, this is homebase for Spark using notebooks',
+      placement: 'right',
+      arrowOffset: 30
+    },
+    {
+      target: 'maintoolbar-container',
       title: 'Welcome to our Jupyter Notebook tl;dr',
       content: 'Hey there! <a href="https://ibm.com/jstart"><img border=0 src="https://raw.githubusercontent.com/joshisa/huemix-blopscotch/master/img/jstart.png" style="height:20px;width:20px;vertical-align:middle;">&nbsp;IBM jStart</a> is honored to be a partner in your journey with Apache Spark, Jupyter Notebooks and OpenStack Object Storage. There\'s plenty of time to read doc and sample code, but join us first on this quick feature fly-by!',
       placement: 'bottom',
-      arrowOffset: 'center'
+      arrowOffset: 0
     }
   ],
   showPrevButton: true,
