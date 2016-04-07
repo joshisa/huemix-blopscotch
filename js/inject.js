376bad6eb5b1f0dbf8dd15e069b0736ac1011a55
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-06 21:32:09 -0400

    Removed github api call because of rate_limit headaches

--- js/inject.js
+++ js/inject.js
@@ -8,47 +8,27 @@ s.onload = function() {
 chrome.extension.sendMessage({}, function(response) {
     var readyStateCheckInterval = setInterval(function() {
       if (document.readyState === "complete") {
-          clearInterval(readyStateCheckInterval);
-          var prefix = "[Huemix Blopscotch]] ";
-          console.log(prefix + "current page URL is: " + location.href);
-          // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
-          proxyXHR.get('https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1').onSuccess(function (data) {
-              var arr = JSON.parse(data);
-              var tourlets = arr.tree.filter(function (el) {
-                  if ('path' in el && typeof(el.path) === 'string' && el.path.indexOf("tourlet/") == 0) {
-                     return true;
-                  }
-              });
-              console.log(prefix + "Number of Tourlets discovered : " + tourlets.length);
-              var i = 0;
-              while (tourlets[i]) {
-                  // https://rawgit.com/joshisa/huemix-blopscotch/master/ + tourlets.path
-                  console.log(prefix + "Loading " + tourlets[i].path);
-                  proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/' + tourlets[i].path).onSuccess(function (data) {
-                        eval(data);
-                  }).onFailure(function (status) {
-                       alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
-                  });
-                  i++;
-              }
-              // Let's only load this stuff once for a given dom
-          }).onFailure(function (status) {
-              tourlets = ["tourlets/demo.js", "tourlets/jstart.js", "tourlets/jupyter.js", "tourlets/spark.js"];
-              console.log(prefix + "Number of Tourlets discovered : " + tourlets.length);
-              var i = 0;
-              while (tourlets[i]) {
-                  // https://rawgit.com/joshisa/huemix-blopscotch/master/ + tourlets.path
-                  console.log(prefix + "Loading " + tourlets[i].path);
-                  proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/' + tourlets[i].path).onSuccess(function (data) {
-                        eval(data);
-                  }).onFailure(function (status) {
-                       alert("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
-                  });
-                  i++;
-              }
-          });
-
-          console.log(prefix + "Hopscotch Dependencies successfully injected");
+        clearInterval(readyStateCheckInterval);
+        var prefix = "[Huemix Blopscotch]] ";
+        console.log(prefix + "current page URL is: " + location.href);
+        // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
+        whitelist = ["tourlets/demo.js", 
+                    "tourlets/jstart.js",
+                    "tourlets/jupyter.js",
+                    "tourlets/spark.js"];
+        console.log(prefix + "Number of Tourlets defined : " + whitelist.length);
+        var i = 0;
+        while (whitelist[i]) {
+            // https://rawgit.com/joshisa/huemix-blopscotch/master/ + tourlets.path
+            console.log(prefix + "Loading " + whitelist[i].path);
+            proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/' + whitelist[i].path).onSuccess(function (data) {
+                eval(data);
+            }).onFailure(function (status) {
+               console.log("HTTP Error " + status + " while retrieving data for the Huemix Blopscotch Tour Chrome Plugin");
+            });
+            i++;
+        }
+        console.log(prefix + "Hopscotch Dependencies successfully injected");
       }
     }, 10);
 });
