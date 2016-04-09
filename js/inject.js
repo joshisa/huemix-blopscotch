c0765f6ba11d7fdeba8663b48acd7dc66ce29145
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-08 23:46:18 -0400

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,10 @@
-proxyXHR.get('https://rawgit.com/joshisa/huemix-blopscotch/master/js/hopscotch.highlight.js').onSuccess(function (data) {
-    eval(data);
-    console.log("[Huemix Blopscotch]] Hopscotch lib loaded!");
-});
+var s = document.createElement('script');
+s.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/js/hopscotch.highlight.js";
+s.onload = function() {
+    this.parentNode.removeChild(this);
+};
+
+(document.head || document.documentElement).appendChild(s);
             
 chrome.extension.sendMessage({}, function(response) {
     var readyStateCheckInterval = setInterval(function() {
