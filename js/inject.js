2dfd2254c9c67acfd038fe4145db381b2ae455a1
Sanjay Joshi <joshisa@us.ibm.com>
2016-04-08 23:47:12 -0400

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,8 +1,8 @@
 var s = document.createElement('script');
 s.src = "https://rawgit.com/joshisa/huemix-blopscotch/master/js/hopscotch.highlight.js";
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
+//s.onload = function() {
+//    this.parentNode.removeChild(this);
+//};
 
 (document.head || document.documentElement).appendChild(s);
             
