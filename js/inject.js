3514f73362c789c04250447c88a494ceeba84430
Sanjay Joshi <joshisa@us.ibm.com>
2019-11-22 00:44:58 -0500

    Updating lastError check

--- js/inject.js
+++ js/inject.js
@@ -7,29 +7,33 @@ s.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/js/hopscotch.h
 (document.head || document.documentElement).appendChild(s);
             
 chrome.extension.sendMessage({}, function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-      if (document.readyState === "complete") {
-        clearInterval(readyStateCheckInterval);
-        var prefix = "[Huemix Blopscotch on githack]] ";
-        console.log(prefix + "current page URL is: " + location.href);
-        // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
-        // Array of registered hoplets
-        whitelist = ["hoplet/demo.js",
-                    "hoplet/jstart.js",
-                    "hoplet/jupyter2.js",
-                    "hoplet/spark.js"];
-                    
-        console.log(prefix + "Number of Hoplets defined : " + whitelist.length);
-        var i = 0;
-        while (whitelist[i]) {
-            // https://raw.githack.com/joshisa/huemix-blopscotch/master/ + whitelist[i]
-            console.log(prefix + "Loading " + whitelist[i]);
-            proxyXHR.get('https://raw.githack.com/joshisa/huemix-blopscotch/master/' + whitelist[i]).onSuccess(function (data) {
-                eval(data);
-            });
-            i++;
+    if (chrome.runtime.lastError) {
+      console.warn("foo::" + chrome.runtime.lastError.message);
+    } else {
+      var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+          clearInterval(readyStateCheckInterval);
+          var prefix = "[Huemix Blopscotch on githack]] ";
+          console.log(prefix + "current page URL is: " + location.href);
+          // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
+          // Array of registered hoplets
+          whitelist = ["hoplet/demo.js",
+                      "hoplet/jstart.js",
+                      "hoplet/jupyter2.js",
+                      "hoplet/spark.js"];
+                      
+          console.log(prefix + "Number of Hoplets defined : " + whitelist.length);
+          var i = 0;
+          while (whitelist[i]) {
+              // https://raw.githack.com/joshisa/huemix-blopscotch/master/ + whitelist[i]
+              console.log(prefix + "Loading " + whitelist[i]);
+              proxyXHR.get('https://raw.githack.com/joshisa/huemix-blopscotch/master/' + whitelist[i]).onSuccess(function (data) {
+                  eval(data);
+              });
+              i++;
+          }
+          console.log(prefix + "Hopscotch Dependencies successfully injected");
         }
-        console.log(prefix + "Hopscotch Dependencies successfully injected");
-      }
-    }, 10);
+      }, 10);
+    }
 });
