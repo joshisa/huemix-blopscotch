79dcbf74335bdaa7e53e0f25039492c2e245c7e5
Sanjay Joshi <joshisa@us.ibm.com>
2019-12-10 00:35:54 -0500

    Updating MCM tour

--- js/inject.js
+++ js/inject.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/js/hopscotch.min.js";
+s.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/js/hopscotch.js";
 //s.onload = function() {
 //    this.parentNode.removeChild(this);
 //};
