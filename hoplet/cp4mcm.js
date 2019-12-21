209f1a94d9fdaa03cf31ca823c39465a156ad2b9
Sanjay Joshi <joshisa@us.ibm.com>
2019-12-20 20:29:49 -0500

    Updating MCM hoplet

--- hoplet/cp4mcm.js
+++ hoplet/cp4mcm.js
@@ -39,4 +39,16 @@ var cp4mcm = /^https?:\/\/icp-console(.*?)/;
     console.log("Is autoplay enabled? " + autoPlayEnabledValue);
     console.log("Global Playback Cadence: " + playBackDelayValue);
   });
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (var key in changes) {
+      var storageChange = changes[key];
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+    }
+  });
 }
