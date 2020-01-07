9afd206f1c254a18d2a2f367108cc0f8da17308e
Sanjay Joshi <joshisa@us.ibm.com>
2020-01-06 22:47:43 -0500

    Update approach to hoplet whitelisting

--- js/inject.js
+++ js/inject.js
@@ -23,6 +23,49 @@ function ping() {
     console.log("Is autoplay enabled? " + autoPlayEnabledValue);
     console.log("Global Playback Cadence: " + playBackDelayValue);
     JSON.parse(categoryValues).forEach(element => console.log("Hoplet Category: " + element + " is enabled."));
+    chrome.extension.sendMessage({}, function(response) {
+        if (chrome.runtime.lastError) {
+          //console.log("foo::" + chrome.runtime.lastError.message);
+          var readyStateCheckInterval = setInterval(function() {
+            if (document.readyState === "complete") {
+              clearInterval(readyStateCheckInterval);
+              var prefix = "[Huemix Blopscotch on githack]] ";
+              console.log(prefix + "current page URL is: " + location.href);
+              // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
+              // Array of registered hoplets
+              /*
+              whitelist = ["hoplet/demo.js",
+                          "hoplet/cp4mcm.js"];
+              */
+             // Trying to make whitelist enumeration dynamic (rather than static list) to accomodate anticipated hoplet growth
+              proxyXHR.get('https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1&client_id=b32b5b5d345ded1f5136&client_secret=84a11f4d715f6a185fda52c3826570346b549a87').onSuccess(function (data) {
+                      var pdata = JSON.parse(data);
+                      var whitelist = [];
+                      for (i in pdata.tree) {
+                        //substringsArray.some(substring=>yourBigString.includes(substring))
+                        //reference:  https://stackoverflow.com/questions/5582574/how-to-check-if-a-string-contains-text-from-an-array-of-substrings-in-javascript#answer-46337280
+                        if (pdata.tree[i].path.indexOf("hoplet/") != -1) {
+                          if JSON.parse(categoryValues).some(substring => pdata.tree[i].path.includes(substring)) {
+                            whitelist.push(pdata.tree[i].path.toString());
+                          }
+                        }
+                      }
+                      console.log(prefix + "Number of Hoplets defined : " + whitelist.length);
+                      var i = 0;
+                      while (whitelist[i]) {
+                          // https://raw.githack.com/joshisa/huemix-blopscotch/master/ + whitelist[i]
+                          console.log(prefix + "Loading " + whitelist[i]);
+                          proxyXHR.get('https://raw.githack.com/joshisa/huemix-blopscotch/master/' + whitelist[i]).onSuccess(function (data) {
+                              eval(data);
+                          });
+                          i++;
+                      }
+              });
+              console.log(prefix + "Hopscotch Dependencies successfully injected");
+            }
+          }, 10);
+        }
+    });
   });
 
   chrome.storage.onChanged.addListener(function(changes, namespace) {
@@ -33,6 +76,8 @@ function ping() {
         changePatrol.setAttribute("data-autoplayenabled", storageChange.newValue);
       } else if (key === "playBackDelay") {
         changePatrol.setAttribute("data-playbackdelay", storageChange.newValue);
+      } else if (key === "categories") {
+        changePatrol.setAttribute("data-categories", storageChange.newValue);
       }
       console.info('Hopscotch for CloudPaks Option "%s" has now been set to "%s"', key, storageChange.newValue);
       /*
@@ -44,46 +89,6 @@ function ping() {
                   storageChange.newValue);
       */
     }
-  });
-
-  chrome.extension.sendMessage({}, function(response) {
-      if (chrome.runtime.lastError) {
-        //console.log("foo::" + chrome.runtime.lastError.message);
-        var readyStateCheckInterval = setInterval(function() {
-          if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
-            var prefix = "[Huemix Blopscotch on githack]] ";
-            console.log(prefix + "current page URL is: " + location.href);
-            // https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1
-            // Array of registered hoplets
-            /*
-            whitelist = ["hoplet/demo.js",
-                        "hoplet/cp4mcm.js"];
-            */
-           // Trying to make whitelist enumeration dynamic (rather than static list) to accomodate anticipated hoplet growth
-            proxyXHR.get('https://api.github.com/repos/joshisa/huemix-blopscotch/git/trees/master?recursive=1&client_id=b32b5b5d345ded1f5136&client_secret=84a11f4d715f6a185fda52c3826570346b549a87').onSuccess(function (data) {
-                    var pdata = JSON.parse(data);
-                    var whitelist = [];
-                    for (i in pdata.tree) {
-                      if (pdata.tree[i].path.indexOf("hoplet/") != -1) {
-                        whitelist.push(pdata.tree[i].path.toString());
-                      }
-                    }
-                    console.log(prefix + "Number of Hoplets defined : " + whitelist.length);
-                    var i = 0;
-                    while (whitelist[i]) {
-                        // https://raw.githack.com/joshisa/huemix-blopscotch/master/ + whitelist[i]
-                        console.log(prefix + "Loading " + whitelist[i]);
-                        proxyXHR.get('https://raw.githack.com/joshisa/huemix-blopscotch/master/' + whitelist[i]).onSuccess(function (data) {
-                            eval(data);
-                        });
-                        i++;
-                    }
-            });
-            console.log(prefix + "Hopscotch Dependencies successfully injected");
-          }
-        }, 10);
-      }
   });
 }
 
