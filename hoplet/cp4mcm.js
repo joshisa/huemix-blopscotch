489a9d4c87e655102680aca9143df83aa03eb124
Sanjay Joshi <joshisa@us.ibm.com>
2020-01-06 21:26:41 -0500

    Update approach to hoplet whitelisting

--- hoplet/cp4mcm.js
+++ hoplet/cp4mcm.js
@@ -1,63 +1,28 @@
-var cp4mcm = /^https?:\/\/icp-console(.*?)/;
+  var cp4mcm = /^https?:\/\/icp-console(.*?)/;
   // Initialize tour if it's the user's first time
   if (cp4mcm.test(location.href)) {
-  console.log(prefix + "Loading tour button on Exploring Cloud Pak for MultiCloud Management");
-  // Let's attach a Take Tour button to the page
-  // <li id="blah"><div id="startTourBtn" style="position:relative;display:inline-block;padding-right:50px;" class="nav-text-button"><a class="button primary" style="width:100%;" href="#">▶ Tour</a></div></li>
-  var create_resource_item = document.getElementById("icp-create-resource");
-  var c = document.createElement('li');
-  c.setAttribute('id', 'itemHolder');
-  var b = document.createElement('div');
-  b.setAttribute('id', 'startTourBtn');
-  b.setAttribute('style', 'position:relative;display:inline-block;padding-right:50px;');
-  var a = document.createElement('a');
-  a.setAttribute('class','button primary');
-  a.setAttribute('style','width:100%;');
-  a.setAttribute('href', '#');
-  var t = document.createTextNode("▶  Tour");
-  a.appendChild(t);
-  b.appendChild(a);
-  c.appendChild(b);
-  create_resource_item.parentNode.insertBefore(c,create_resource_item);
-  console.log(prefix + "Tour Button injected ...");
-  console.log(prefix + "Happy Touring from the IBM CP4MCM Team!");
+    console.log(prefix + "Loading tour button on Exploring Cloud Pak for MultiCloud Management");
+    // Let's attach a Take Tour button to the page
+    // <li id="blah"><div id="startTourBtn" style="position:relative;display:inline-block;padding-right:50px;" class="nav-text-button"><a class="button primary" style="width:100%;" href="#">▶ Tour</a></div></li>
+    var create_resource_item = document.getElementById("icp-create-resource");
+    var c = document.createElement('li');
+    c.setAttribute('id', 'itemHolder');
+    var b = document.createElement('div');
+    b.setAttribute('id', 'startTourBtn');
+    b.setAttribute('style', 'position:relative;display:inline-block;padding-right:50px;');
+    var a = document.createElement('a');
+    a.setAttribute('class','button primary');
+    a.setAttribute('style','width:100%;');
+    a.setAttribute('href', '#');
+    var t = document.createTextNode("▶  Tour");
+    a.appendChild(t);
+    b.appendChild(a);
+    c.appendChild(b);
+    create_resource_item.parentNode.insertBefore(c,create_resource_item);
+    console.log(prefix + "Tour Button injected ...");
+    console.log(prefix + "Happy Touring from the IBM CP4MCM Team!");
 
-  var t = document.createElement ('script');
-  t.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/tour/cp4mcm.js";
-  document.body.appendChild(t);
-
-  chrome.storage.sync.get(['autoPlayEnabled','playBackDelay'], function(items) {
-    autoPlayEnabledValue = items.autoPlayEnabled || "false";
-    playBackDelayValue = items.playBackDelay || "8";
-    //Trying to create this hidden div to share extension options with tour script
-    //<div id="hopscotchOptions" data-autoplayenabled="true" data-playbackdelay="8"></div>
-    var h = document.createElement('div');
-    h.id = 'hopscotchOptions';
-    h.setAttribute("data-autoplayenabled", autoPlayEnabledValue);
-    h.setAttribute("data-playbackdelay", playBackDelayValue);
-    document.body.appendChild(h);
-    console.log("Is autoplay enabled? " + autoPlayEnabledValue);
-    console.log("Global Playback Cadence: " + playBackDelayValue);
-  });
-
-  chrome.storage.onChanged.addListener(function(changes, namespace) {
-    for (var key in changes) {
-      var storageChange = changes[key];
-      var changePatrol = document.getElementById("hopscotchOptions");
-      if (key === "autoPlayEnabled") {
-        changePatrol.setAttribute("data-autoplayenabled", storageChange.newValue);
-      } else if (key === "playBackDelay") {
-        changePatrol.setAttribute("data-playbackdelay", storageChange.newValue);
-      }
-      console.info('Hopscotch for CloudPaks Option "%s" has now been set to "%s"', key, storageChange.newValue);
-      /*
-      console.log('Storage key "%s" in namespace "%s" changed. ' +
-                  'Old value was "%s", new value is "%s".',
-                  key,
-                  namespace,
-                  storageChange.oldValue,
-                  storageChange.newValue);
-      */
-    }
-  });
-}
+    var t = document.createElement ('script');
+    t.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/tour/cp4mcm.js";
+    document.body.appendChild(t);
+  }
--- js/inject.js
+++ js/inject.js
@@ -7,6 +7,45 @@ s.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/js/hopscotch.j
 (document.head || document.documentElement).appendChild(s);
             
 function ping() {
+
+  chrome.storage.sync.get(['autoPlayEnabled','playBackDelay','categories'], function(items) {
+    autoPlayEnabledValue = items.autoPlayEnabled || "false";
+    playBackDelayValue = items.playBackDelay || "8";
+    categoryValues = items.categories || JSON.stringify(['default']);
+    //Trying to create this hidden div to share extension options with tour script
+    //<div id="hopscotchOptions" data-autoplayenabled="true" data-playbackdelay="8"></div>
+    var h = document.createElement('div');
+    h.id = 'hopscotchOptions';
+    h.setAttribute("data-autoplayenabled", autoPlayEnabledValue);
+    h.setAttribute("data-playbackdelay", playBackDelayValue);
+    h.setAttribute("data-categories", categoryValues);
+    document.body.appendChild(h);
+    console.log("Is autoplay enabled? " + autoPlayEnabledValue);
+    console.log("Global Playback Cadence: " + playBackDelayValue);
+    JSON.parse(categoryValues).forEach(element => console.log("Hoplet Category: " + element + " is enabled."));
+  });
+
+  chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (var key in changes) {
+      var storageChange = changes[key];
+      var changePatrol = document.getElementById("hopscotchOptions");
+      if (key === "autoPlayEnabled") {
+        changePatrol.setAttribute("data-autoplayenabled", storageChange.newValue);
+      } else if (key === "playBackDelay") {
+        changePatrol.setAttribute("data-playbackdelay", storageChange.newValue);
+      }
+      console.info('Hopscotch for CloudPaks Option "%s" has now been set to "%s"', key, storageChange.newValue);
+      /*
+      console.log('Storage key "%s" in namespace "%s" changed. ' +
+                  'Old value was "%s", new value is "%s".',
+                  key,
+                  namespace,
+                  storageChange.oldValue,
+                  storageChange.newValue);
+      */
+    }
+  });
+
   chrome.extension.sendMessage({}, function(response) {
       if (chrome.runtime.lastError) {
         //console.log("foo::" + chrome.runtime.lastError.message);
