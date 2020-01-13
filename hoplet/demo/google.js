bfe340afb40f5e04deec4e7a064ab07579a3b0b0
Sanjay Joshi <joshisa@us.ibm.com>
2020-01-13 11:36:47 -0500

    Update google demo

--- hoplet/demo/google.js
+++ hoplet/demo/google.js
@@ -1,21 +1,23 @@
   var goog = /^https?:\/\/(.*?)google(.*?)/;
-  // Initialize tour if it's the user's first time
+  // Author choice to narrow down to a specific url
   if (goog.test(location.href)) {
-    console.log(prefix + "Loading tour button onto Google web page");
-    // Let's attach a Take Tour button to the page
-    // <li id="blah"><div id="startTourBtn" style="position:relative;display:inline-block;padding-right:50px;" class="nav-text-button"><a class="button primary" style="width:100%;" href="#">▶ Tour</a></div></li>
-    var googtophf = document.getElementById("tophf");
-    var a = document.createElement('input');
-    a.setAttribute('id', 'googtour');
-    a.setAttribute('aria-label','Google Tour');
-    a.setAttribute('name', 'googtour');
-    a.setAttribute('type', 'submit');
-    a.setAttribute('value', 'Web Tour');
-    googtophf.append(a);
-    console.log(prefix + "Tour Button injected on Google WebSite...");
-    console.log(prefix + "Happy Touring from the IBM CP4MCM Team!");
+    document.addEventListener("DOMContentLoaded", function(){
+      console.log(prefix + "Loading tour button onto Google web page");
+      // Let's attach a Take Tour button to the page
+      // <li id="blah"><div id="startTourBtn" style="position:relative;display:inline-block;padding-right:50px;" class="nav-text-button"><a class="button primary" style="width:100%;" href="#">▶ Tour</a></div></li>
+      var googtophf = document.getElementById("tophf");
+      var a = document.createElement('input');
+      a.setAttribute('id', 'googtour');
+      a.setAttribute('aria-label','Google Tour');
+      a.setAttribute('name', 'googtour');
+      a.setAttribute('type', 'submit');
+      a.setAttribute('value', 'Web Tour');
+      googtophf.append(a);
+      console.log(prefix + "Tour Button injected on Google WebSite...");
+      console.log(prefix + "Happy Touring from the IBM CP4MCM Team!");
 
-    var t = document.createElement ('script');
-    t.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/tour/google.js";
-    document.body.appendChild(t);
+      var t = document.createElement ('script');
+      t.src = "https://raw.githack.com/joshisa/huemix-blopscotch/master/tour/google.js";
+      document.body.appendChild(t);
+    });
   }
